c376b1982bf124e6a044c29ed4ea097e735b282b
NehaAR25 <nehabenribadiya@gmail.com>
2023-07-01 15:25:51 +0100

    my test

---
+++ src/test/java/com/mytest/testbase/BaseTest.java
@@ -0,0 +1,18 @@
+package com.mytest.testbase;
+
+import com.mytest.utilities.Utility;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest extends Utility {
+    String browser = "Chrome";
+    @BeforeMethod
+    public void setUp(){
+        selectBrowser(browser);
+
+    }
+    @AfterMethod
+    public void tearDown(){
+        closeBrowser();
+    }
+}
